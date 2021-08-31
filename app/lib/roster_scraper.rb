bd9d21fb7a5917b6becf2b1c01a2bc0c21377e9d
Jesse Farmer <jessethebuilder@yahoo.com>
2021-08-31 12:10:39 -0700

    Chrome settings for Heroku

--- app/lib/roster_scraper.rb
+++ app/lib/roster_scraper.rb
@@ -2,7 +2,23 @@ class RosterScraper
   INDEX_URL = 'https://websrv23.clallam.net/NewWorld.InmateInquiry/WA0050000?Page='
 
   def initialize
-    @ghost = Capybara::Session.new(:selenium_chrome_headless)
+    if Rails.env.production?
+      chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
+
+      chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
+
+      Capybara.register_driver :chrome do |app|
+        Capybara::Selenium::Driver.new(
+          app,
+          browser: :chrome,
+          desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
+        )
+      end
+
+      @ghost = Capybara::Session.new(:chrome)
+    else
+      @ghost = Capybara::Session.new(:selenium_chrome_headless)
+    end
   end
 
   def scrape
